14759e2641d168083aad64bf9ec9a71d1dea515e
Nitin Singh <nitin27may@gmail.com>
2020-03-12 14:39:03 -0400

    Folder name updated, Loadbalancer implementation

--- README.md
+++ README.md
@@ -1,31 +1,33 @@
-# MEAN Stack using Docker, Docker Images and Docker Compose
+# MEAN Stack using Docker
 
 MEAN (full) stack application which comprises of MongoDB, ExpressJS, Angular and NodeJS. MongoDB takes care of the back end database, NodeJS and ExpressJS are for server side rendering, rest apis, and Angular is for front end.
 
 Docker Compose can be used to create separate containers (and host them) for each of the stacks in a MEAN stack application. MEAN is the acronym for MongoDB Express Angular & NodeJs
 
-## Mongo DB Image
+## Angular
 
-Download mongodb image: `docekr pull mongo`
+frontend folder contains the Angular App
 
-Update the connection string like below:
+## Expressjs
+
+Api folder contains REST apis which is developed using expressjs
+
+## Mongo DB Connection
+
+Update the connection string, in api/server.js like below:
+
+## NGINX
+
+We have uses NGINX loadbalancer in case if there is a requirement that frontend and api need to be exposed on same port. For configutration please check loadbalancer/nginx.conf
 
 if using mongo db image :
 `"connectionString":"mongodb://database/mean-docker"`
 
 If running mongodb remotely (Like hosted db on mlab) :
-`"connectionString": "mongodb://username:password@ds056789.mlab.com:56789/nksmongo"`
+`"connectionString": "mongodb://username:password@ds056789.mlab.com:56789/dbName"`
 
-## Angular
-
-AngularClient folder contains the Angular UI App
-
-## NodeJs Image
-
-Download mongodb Nodejs: `docekr pull node:10.7.0-alpine`
-
-## Docker Compose
+## How to run project
 
 To run the project run below command:
 
-`docker-compose up --build`
+`docker-compose up`
--- frontend/server.ts
+++ frontend/server.ts
@@ -15,50 +15,45 @@
  * import for `ngExpressEngine`.
  */
 
-import "zone.js/dist/zone-node";
+import 'zone.js/dist/zone-node';
 
-import * as express from "express";
-import { join } from "path";
+import * as express from 'express';
+import { join } from 'path';
 
 // Express server
 const app = express();
 
-const PORT = process.env.PORT || 8000;
-const DIST_FOLDER = join(process.cwd(), "dist/browser");
+const PORT = process.env.PORT || 4000;
+const DIST_FOLDER = join(process.cwd(), 'dist/browser');
 
 // * NOTE :: leave this as require() since this file is built Dynamically from webpack
-const {
-  AppServerModuleNgFactory,
-  LAZY_MODULE_MAP,
-  ngExpressEngine,
-  provideModuleMap
-} = require("./dist/server/main");
+const { AppServerModuleNgFactory, LAZY_MODULE_MAP, ngExpressEngine, provideModuleMap } = require('./dist/server/main');
 
 // Our Universal express-engine (found @ https://github.com/angular/universal/tree/master/modules/express-engine)
 app.engine(
-  "html",
+  'html',
   ngExpressEngine({
     bootstrap: AppServerModuleNgFactory,
     providers: [provideModuleMap(LAZY_MODULE_MAP)]
   })
 );
 
-app.set("view engine", "html");
-app.set("views", DIST_FOLDER);
+app.set('view engine', 'html');
+app.set('views', DIST_FOLDER);
 
 // Example Express Rest API endpoints
 // app.get('/api/**', (req, res) => { });
 // Serve static files from /browser
 app.get(
-  "*.*",
+  '*.*',
   express.static(DIST_FOLDER, {
-    maxAge: "1y"
+    maxAge: '1y'
   })
 );
 
 // All regular routes use the Universal engine
-app.get("*", (req, res) => {
-  res.render("index", { req });
+app.get('*', (req, res) => {
+  res.render('index', { req });
 });
 
 // Start up the Node server
