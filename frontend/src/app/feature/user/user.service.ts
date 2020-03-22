8352adc923daa682d2919577f57bb664b2c760fe
Nitin Singh <nitin27may@gmail.com>
2020-03-22 01:44:34 -0400

    Angular upgrade, UI changes and new contact api updates

--- frontend/src/app/feature/user/user.service.ts
+++ frontend/src/app/feature/user/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { map } from "rxjs/operators";
 import { environment } from "../../../environments/environment";
-import { User } from "../models/user.interface";
+import { User } from "./user.model";
 
 @Injectable()
 export class UserService {
