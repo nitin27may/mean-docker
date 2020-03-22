20ba18a1ca06e89b987ebf3f5e7e0cc1135a6740
Nitin Singh <nitin27may@gmail.com>
2020-03-22 01:44:34 -0400

    Angular upgrade, UI changes and new contact api updates

--- frontend/src/app/feature/user/user.model.ts
+++ frontend/src/app/feature/user/user.model.ts
@@ -1,7 +1,10 @@
-﻿export class User {
+export class User {
   _id: string;
   username: string;
   password: string;
   firstName: string;
   lastName: string;
+  token: string;
+  email: string;
+  mobile: string;
 }
