27acb658e08562e1e3cf6d568d681a81bf9e1064
Nitin Singh <nitin27may@gmail.com>
2020-04-17 01:06:08 -0400

    Adding grid module, removing user model: 1. renaming model to interface. 2. Grid module added to contact module 3. Updated Contacts routing 4. Fixed contacts link in siderbar

--- frontend/src/app/core/services/user.service.ts
+++ frontend/src/app/core/services/user.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from "@angular/core";
 import { HttpClient } from "@angular/common/http";
 import { map } from "rxjs/operators";
 import { environment } from "../../../environments/environment";
-import { User } from "../models/user.model";
+import { User } from "../models/user.interface";
 
 @Injectable()
 export class UserService {
