34d072aee1bfe66ea95b68d225593fb6d7516654
Nitin Singh <nitin27may@gmail.com>
2022-11-19 19:04:44 -0500

    angular 15 upgrade and dockerfile update

--- frontend/src/app/app-routing.module.ts
+++ frontend/src/app/app-routing.module.ts
@@ -17,7 +17,7 @@ const routes: Routes = [
 @NgModule({
   imports: [
     RouterModule.forRoot(routes, {
-      relativeLinkResolution: "legacy"
+      // relativeLinkResolution: "legacy"
     })
   ],
   exports: [RouterModule]
